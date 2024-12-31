0ad408d5a19fd5320bcdb288a2a13990770bbe20
JuhainaMT <juhaina@mvp-apps.ae>
2024-12-31 16:15:00 +0530

    Add filter for trainers

--- src/sections/overview/e-commerce/ecommerce-total-session.tsx
+++ src/sections/overview/e-commerce/ecommerce-total-session.tsx
@@ -1,11 +1,12 @@
 import { ApexOptions } from 'apexcharts';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card, { CardProps } from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import Chart, { useChart } from 'src/components/chart';
 import { useGetTrainerInsights } from 'src/api/anlytics';
+import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 interface Props extends CardProps {
   title?: string;
@@ -14,10 +15,15 @@ interface Props extends CardProps {
 
 export default function TotalTrainersSession({ title, subheader, ...other }: Props) {
   const theme = useTheme();
-
   const { trainerInsights, trainerInsightsLoading } = useGetTrainerInsights();
   const sessionsPerTrainer = trainerInsights?.sessionsPerTrainer || [];
 
+  const [selectedTrainer, setSelectedTrainer] = useState<string>('');
+  useEffect(() => {
+    if (sessionsPerTrainer.length > 0) {
+      setSelectedTrainer(sessionsPerTrainer[0].trainer_name);
+    }
+  }, [sessionsPerTrainer]);
   // Prepare chart data
   const chart = useMemo(() => {
     const months = [
@@ -50,6 +56,7 @@ export default function TotalTrainersSession({ title, subheader, ...other }: Pro
 
     return { months, series };
   }, [sessionsPerTrainer]);
+
   const chartOptions = useChart({
     chart: {
       type: 'bar',
@@ -84,6 +91,10 @@ export default function TotalTrainersSession({ title, subheader, ...other }: Pro
     },
   });
 
+  const filteredSeries = selectedTrainer
+    ? chart.series.filter((trainer) => trainer.name === selectedTrainer)
+    : chart.series;
+
   if (trainerInsightsLoading) {
     return (
       <Card {...other}>
@@ -97,7 +108,30 @@ export default function TotalTrainersSession({ title, subheader, ...other }: Pro
     <Card {...other}>
       <CardHeader title={title} subheader={subheader} />
       <Box sx={{ mt: 3, mx: 3 }}>
-        <Chart dir="ltr" type="bar" series={chart.series} options={chartOptions} height={394} />
+        <Box sx={{ mt: 3, mx: 3, display: 'flex', justifyContent: 'flex-end' }}>
+          {' '}
+          <FormControl sx={{ mb: 2, width: 200 }}>
+            <InputLabel id="trainer-select-label">Select Trainer</InputLabel>
+            <Select
+              labelId="trainer-select-label"
+              value={selectedTrainer}
+              onChange={(e) => setSelectedTrainer(e.target.value)}
+              label="Select Trainer"
+            >
+              <MenuItem value="" disabled>
+                <em>All Trainers</em>
+              </MenuItem>
+
+              {sessionsPerTrainer.map((trainer) => (
+                <MenuItem key={trainer.trainer_name} value={trainer.trainer_name}>
+                  {trainer.trainer_name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Chart dir="ltr" type="bar" series={filteredSeries} options={chartOptions} height={394} />
       </Box>
     </Card>
   );
