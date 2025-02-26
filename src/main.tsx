623948a9736c87af8047dbc6a259c24375195d95
JuhainaMT <juhaina@mvp-apps.ae>
2025-02-26 13:40:08 +0530

    Integrated the sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import * as Sentry from '@sentry/react';
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+
 //
 import App from './App';
 import { GoogleMapsProvider } from './sections/overview/e-commerce/GoogleMapsProvider';
@@ -9,6 +11,17 @@ import { GoogleMapsProvider } from './sections/overview/e-commerce/GoogleMapsPro
 // ----------------------------------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+Sentry.init({
+  dsn: 'https://1348ccae390e48bf837387f039b58844@o4507516184821760.ingest.us.sentry.io/4508884439793664',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 root.render(
   <HelmetProvider>
