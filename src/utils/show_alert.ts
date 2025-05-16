d59b6de0c4a40bf24fe2926098196609be131d12
Aysha <aysha@mvp-apps.ae>
2025-05-16 15:23:50 +0400

    Add SweetAlert2 for enhanced notification alerts and update notification handling

---
+++ src/utils/show_alert.ts
@@ -0,0 +1,23 @@
+import Swal from 'sweetalert2';
+
+export function showAlert(title: any, body: any) {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    customClass: {
+      container: 'custom-toast-container',
+    },
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer);
+      toast.addEventListener('mouseleave', Swal.resumeTimer);
+    },
+  });
+  Toast.fire({
+    icon: 'success',
+    title: title,
+    html: body,
+  });
+}
